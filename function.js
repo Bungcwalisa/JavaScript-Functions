07fd87f4ac5c126c2b5cf7fbf8709d1cbfe83640
Bungcwalisa <magobiyanebungcwalisa.com>
2025-11-04 09:55:37 +0200

    JavaScript Functions

---
+++ function.js
@@ -0,0 +1,13 @@
+function greet(name) {
+    return `Hello ${name}!`;
+}
+
+function add(a, b) {
+    return a + b;
+}
+
+let greeting = greet("Bungcwalisa");
+let sum = add (20, 1);
+
+console.log(greeting);
+console.log(sum);
